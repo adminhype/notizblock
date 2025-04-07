b1dc33b8d367bf5b864c0b27f885e93f9d8a72d0
Adem <ado.designs@outlook.de>
2025-04-08 00:26:52 +0200

    update templates, stylings

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 // notizen anzeigen lassen
 
-let notesTitles = ['ba', 'aufgabe'];
-let notes = ['notiz', 'neee'];
+let notesTitles = [];
+let notes = [];
 
 let trashNotesTitles = [];
 let trashNotes = [];
+
+let archivNotesTitles = [];
+let archivNotes = [];
 
 function renderNotes() {
     let contentRef = document.getElementById('content')
@@ -14,7 +17,6 @@ function renderNotes() {
         contentRef.innerHTML += getNoteTemplate(indexNote);
     }
 }
-
 // vorhandene funktion kopieren ändern um gelösche array inhalte anzuzeigen
 function renderTrashNotes() {
     let trashContentRef = document.getElementById('trash_content')
@@ -24,13 +26,20 @@ function renderTrashNotes() {
         trashContentRef.innerHTML += getTrashNoteTemplate(indexTrashNote);
     }
 }
+function renderArchivNotes() {
+    let ArchivContentRef = document.getElementById('archiv_content')
+    ArchivContentRef.innerHTML = "";
 
+    for (let indexArchivNote = 0; indexArchivNote < archivNotes.length; indexArchivNote++) {
+        ArchivContentRef.innerHTML += getTrashNoteTemplate(indexArchivNote);
+    }
+}
 
 // notizen hinzufügen
 
 function addNote() {
     // eingabe anzeigen lassen
-    let NoteInputRef = document.getElementById('note_input')
+    let NoteInputRef = document.getElementById('note_input_title')
     // eingabe auslesen
     let noteInput = NoteInputRef.value;
     // eingabe den notzien hinzufügen  
@@ -43,11 +52,25 @@ function addNote() {
     //werte zurück auf leeren string setzen
     NoteInputRef.value = "";
 }
+function addNote() {
+    // eingabe anzeigen lassen
+    let NoteInputRef = document.getElementById('note_input_content')
+    // eingabe auslesen
+    let noteInput = NoteInputRef.value;
+    // eingabe den notzien hinzufügen  
+    notes.push(noteInput)
 
+
+    // function anzeigen lassen
+    renderNotes();
+
+    //werte zurück auf leeren string setzen
+    NoteInputRef.value = "";
+}
 // welche notiz muss gelöscht werden (indexNote)
 
 //notizen löschen
-function AddTrashNote(indexNote) {
+function noteToTrash(indexNote) {
     let trashNote = notes.splice(indexNote, 1);
     trashNotes.push(trashNote[0]);
 
--- scripts/templates.js
+++ scripts/templates.js
@@ -3,10 +3,46 @@ function getNoteTemplate(indexNote) {
     // wann muss die notziz gelöscht werden ?
     // wenn wir auf den button klicken
     // notestitle array hinzufügt um titel auch in trash anzeigen zulassen
-    return `<p>+ title: ${notesTitles[indexNote]} -> ${notes[indexNote]}<button onclick="AddTrashNote(${indexNote})">x</button> </p>`;
+    return /*html*/`
+    <div class="note">
+        <h3>${notesTitles[indexNote]}</h3>
+        <p>${notes[indexNote]}</p>
+        <div>
+            <button class="btn" onclick="noteToTrash(${indexNote})">X</button>
+            <button class="btn" onclick="noteToArchiv(${indexNote})">A</button>
+        </div>
+    </div>
+    `;
 }
-
-
+function getArchivNoteTemplate(indexArchivNote) {
+    // wann muss die notziz gelöscht werden ?
+    // wenn wir auf den button klicken
+    // notestitle array hinzufügt um titel auch in trash anzeigen zulassen
+    return /*html*/`
+    <div class="note">
+        <h3>${archivNotesTitles[indexArchivNote]}</h3>
+        <p>${archivNotes[indexArchivNote]}</p>
+        <div>
+            <button class="btn" onclick="noteToTrash(${indexArchivNote})">X</button>
+            <button class="btn" onclick="noteToArchiv(${indexArchivNote})">A</button>
+        </div>
+    </div>
+    `;
+}
 function getTrashNoteTemplate(indexTrashNote) {
-    return `<p>+ title: ${trashNotesTitles[indexTrashNote]} -> ${trashNotes[indexTrashNote]}<button onclick="deleteNote(${indexTrashNote})">löschen</button> </p>`;
+    // wann muss die notziz gelöscht werden ?
+    // wenn wir auf den button klicken
+    // notestitle array hinzufügt um titel auch in trash anzeigen zulassen
+    return /*html*/`
+    <div class="note">
+        <h3>${trashNotesTitles[indexTrashNote]}</h3>
+        <p>${trashNotes[indexTrashNote]}</p>
+        <div>
+            <button class="btn" onclick="noteToTrash(${indexTrashNote})">X</button>
+            <button class="btn" onclick="noteToArchiv(${indexTrashNote})">A</button>
+        </div>
+    </div>
+    `;
 }
+
+
